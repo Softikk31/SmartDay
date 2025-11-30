15ae2e7d0ab9e6c2f87cb6afd31100b743cb9ac4
Softikk <softikk31@gmail.com>
2025-11-30 19:54:27 +0300

    chore: Update app version to 1.3.1-beta
    Updates the `versionName` from "1.4-beta" to "1.3.1-beta".

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         minSdk = 26
         targetSdk = 36
         versionCode = 1
-        versionName = "1.4-beta"
+        versionName = "1.3.1-beta"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
