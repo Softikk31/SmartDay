43bd6d18ca7d17c6e47c37ec18ce472a78d587cf
Softikk <softikk31@gmail.com>
2025-12-01 11:43:52 +0300

    chore: Bump version to 1.3.2-beta
    Updates the application version:
    *   `versionCode` is incremented from 2 to 3.
    *   `versionName` is changed from "1.3.1-beta" to "1.3.2-beta".

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "dev.smartday.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.3.1-beta"
+        versionCode = 3
+        versionName = "1.3.2-beta"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
